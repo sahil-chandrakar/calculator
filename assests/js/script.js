3f65f82ed4f14f3dcd172c2176c6407ec2218920
sahil-chandrakar <chandrakarsahil2002@gmail.com>
2025-11-24 14:51:36 +0530

    refactor project-structure

---
+++ assests/js/script.js
@@ -0,0 +1,20 @@
+let string = "";
+let buttons = document.querySelectorAll(".btn");
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("#result").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      document.querySelector("#result").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = string.slice(0, string.length - 1);
+      document.querySelector("#result").value = string;
+    } else {
+      string += e.target.innerHTML;
+      document.querySelector("#result").value = string;
+    }
+  });
+});
